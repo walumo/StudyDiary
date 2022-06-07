cc0f5ddc29cab3bbb213919cf26c702553e5ad49
walumo <miikka.hakulinen@awacademy.training>
2022-06-07 15:20:29 +0300

    Removed redundant code

--- Task.cs
+++ Task.cs
@@ -10,7 +10,6 @@ namespace StudyDiary
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        //public List<string> Notes { get; set; }
         public DateTime Deadline { get; set; }
         public bool Done { get; set; }
 
