d246670eb8637ec00adb31494bdb11a66e9d2bb4
walumo <miikka.hakulinen@awacademy.training>
2022-06-08 16:59:07 +0300

    Added Load()

---
+++ logic/Load.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyDiary
+{
+    class Load
+    {
+        public static List<Topic> LoadAll()
+        {
+            List<Topic> buffer = new List<Topic>();
+
+            foreach (string item in File.ReadAllLines(Environment.CurrentDirectory + @"\topics\topic.txt").ToList())
+            {
+                string str = (JsonConvert.SerializeObject(item));
+                Topic dsTopic = JsonConvert.DeserializeObject<Topic>(str);
+                buffer.Add(dsTopic);
+            }
+            return buffer;
+        }
+    }
+}
